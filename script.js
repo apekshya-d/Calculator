32d3a3af7112b69bb483816f644be31574d13a5b
Apekshya Dhakal <apekshya.d@gmail.com>
2022-10-27 17:48:35 -0400

    +/- button added secondary display done

--- script.js
+++ script.js
@@ -1,52 +1,86 @@
 let numbers = Array.from(document.querySelectorAll('.numbers'));
-let display = document.querySelector('#display');
+let primaryDisplay = document.querySelector('#primaryDisplay');
+let secondaryDisplay = document.querySelector('#secondaryDisplay');
 let operators = Array.from(document.querySelectorAll('.operator'));
 let equalsTo = document.querySelector('#equalsTo');
 let clearButton = document.querySelector('#clearButton');
+let plusMinus = document.querySelector('#plusMinus');
 
 let a; //currentNumber
 let b; // nextNumber
 let selectedOperator; // operator
 let lastPressed = '';
+let history = [];
 
 numbers.forEach((number)=>{
     number.addEventListener('click',(e)=>{
         if( lastPressed === '' || lastPressed === 'number'){
-        display.textContent += e.target.textContent;
+        primaryDisplay.textContent += e.target.textContent;
         }else{
-            display.textContent = e.target.textContent;
+            primaryDisplay.textContent = e.target.textContent;
         }
         lastPressed = 'number';
+        history.push(e.target.textContent);
+        secondaryDisplay.textContent = history.join('');
     })
 
 });
 
+plusMinus.addEventListener('click', (e)=>{
+   if(primaryDisplay.textContent > 0 && lastPressed === 'number'){
+    primaryDisplay.textContent = -Math.abs(primaryDisplay.textContent);
+    history.splice(history.length-1, 1, primaryDisplay.textContent);
+    secondaryDisplay.textContent = history.join('');
+   }
+   else if(primaryDisplay.textContent <= 0 && lastPressed === 'number'){
+    primaryDisplay.textContent = Math.abs(primaryDisplay.textContent);
+    history.splice(history.length-1, 1, primaryDisplay.textContent);
+    secondaryDisplay.textContent = history.join('');
+   }
+})
+
 operators.forEach((operator)=>{
     operator.addEventListener('click',(e) => {
+        if (lastPressed !== 'number' && lastPressed !== 'operator') {
+            return;
+        } else if (lastPressed === 'operator'){
+            history.splice(history.length-1, 1,` ${e.target.textContent} `);
+            secondaryDisplay.textContent = history.join('');  
+        } else if (lastPressed === 'number') {
             if (typeof a === "undefined"){
-                a = display.textContent;
-            }else {
-                b = display.textContent;
+                a = primaryDisplay.textContent;
+            } else {
+                b = primaryDisplay.textContent;
                 a = operate(selectedOperator, a, b);
-                display.textContent = a;
+                primaryDisplay.textContent = a;
             }
-            lastPressed = 'operator';
-            selectedOperator = e.target.textContent;
-        })
+
+            history.push(` ${e.target.textContent} `);
+            secondaryDisplay.textContent = history.join('');
+        }     
+        selectedOperator = e.target.textContent;
+        lastPressed = 'operator';
+            
+     })
     
 });
 
-equalsTo.addEventListener('click', function () {
-    b = display.textContent;
-    result = operate(selectedOperator, a, b);
-    lastPressed =  'equalsTo';
-    display.textContent = result;
-})
+equalsTo.addEventListener('click', (e) => {
+        b = primaryDisplay.textContent;
+        result = operate(selectedOperator, a, b);
+        lastPressed = 'equalsTo';
+        primaryDisplay.textContent = result;
+        
+    })
 
 clearButton.addEventListener('click', () => {
-    display.textContent = '';
-    a = 0;
-    b = 0;
+    primaryDisplay.textContent = '';
+    secondaryDisplay.textContent = '';
+    history = [];
+    a = undefined;
+    b = undefined;
+    selectedOperator = undefined;
+    lastPressed = '';
 
 })
 
